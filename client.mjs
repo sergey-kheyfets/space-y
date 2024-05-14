80bbbb69d217905ec13a479ff2e9aeaed3c49d5e
sergey.kheyfets <telegram only>
2024-05-14 20:07:17 +0500

    мяяяяяу

--- client.mjs
+++ client.mjs
@@ -1,186 +1,251 @@
 export class Client {
-  /**
-   * Должен возвращать имя пользователя или null
-   * если пользователь не залогинен
-   *
-   * @return {Promise<string | null>} username
-   * */
-  async getUser() {
-    throw new Error("Not implemented");
-  }
+    async getUser() {
+        const response = await fetch('/api/user');
+        const userData = await response.json();
+        return userData.username;
+    }
 
-  /**
-   * Должен логинить пользователя с именем username
-   * и возвращать его имя
-   *
-   * @param {string} username
-   * @return {Promise<string | null>} username
-   * */
-  async loginUser(username) {
-    throw new Error("Not implemented");
-  }
+    async loginUser(username) {
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username}),
+        });
+        const userData = await response.json();
+        return userData.username;
+    }
 
-  /**
-   * Должен разлогинивать текущего пользователя
-   *
-   * @return {void}
-   * */
-  async logoutUser() {
-    throw new Error("Not implemented");
-  }
+    async logoutUser() {
+        await fetch('/api/logout', {
+            method: 'POST',
+        });
+    }
 
-  /**
-   * Должен возвращать информацию о компании
-   *
-   * @typedef {Object} Headquarters
-   * @property {string} address
-   * @property {string} city
-   * @property {string} state
-   *
-   * @typedef {Object} About
-   * @property {string} founder
-   * @property {string} founded
-   * @property {number} employees
-   * @property {string} ceo
-   * @property {string} coo
-   * @property {string} cto
-   * @property {number} valuation
-   * @property {Headquarters} headquarters
-   * @property {string} summary
-   * @return {Promise<About>}
-   * */
-  async getInfo() {
-    throw new Error("Not implemented");
-  }
+    /**
+     * Должен возвращать информацию о компании
+     *
+     * @typedef {Object} Headquarters
+     * @property {string} address
+     * @property {string} city
+     * @property {string} state
+     *
+     * @typedef {Object} About
+     * @property {string} founder
+     * @property {string} founded
+     * @property {number} employees
+     * @property {string} ceo
+     * @property {string} coo
+     * @property {string} cto
+     * @property {number} valuation
+     * @property {Headquarters} headquarters
+     * @property {string} summary
+     * @return {Promise<About>}
+     * */
+    async getInfo() {
+        const response = await fetch('https://api.spacexdata.com/v4/company');
+        const data = await response.json();
+        return {
+            founder: data.founder,
+            founded: data.founded,
+            employees: data.employees,
+            ceo: data.ceo,
+            coo: data.coo,
+            cto: data.cto,
+            valuation: data.valuation,
+            headquarters: {
+                address: data.headquarters.address,
+                city: data.headquarters.city,
+                state: data.headquarters.state
+            },
+            summary: data.summary
+        };
+    }
 
-  /**
-   * Должен возвращать информацию о всех событиях
-   *
-   * @typedef {Object} EventBrief
-   * @property {number} id
-   * @property {string} title
-   *
-   * @return {Promise<EventBrief[]>}
-   * */
-  async getHistory() {
-    throw new Error("Not implemented");
-  }
+    /**
+     * Должен возвращать информацию о всех событиях
+     *
+     * @typedef {Object} EventBrief
+     * @property {number} id
+     * @property {string} title
+     *
+     * @return {Promise<EventBrief[]>}
+     * */
+    async getHistory() {
+        const response = await fetch('https://api.spacexdata.com/v4/history');
+        const data = await response.json();
 
-  /**
-   * Должен возвращать информацию о запрошенном событии
-   *
-   * @typedef {Object} EventFull
-   * @property {number} id
-   * @property {string} title
-   * @property {string} event_date_utc
-   * @property {string} details
-   * @property {Object.<string, ?string>} links
-   *
-   * @param {number} id
-   * @return {Promise<EventFull>}
-   * */
-  async getHistoryEvent(id) {
-    throw new Error("Not implemented");
-  }
+        const eventBriefs = data.map(event => ({
+            id: event.id,
+            title: event.title
+        }));
 
-  /**
-   * Должен возвращать информацию о всех ракетах
-   *
-   * @typedef {Object} RocketBrief
-   * @property {number} rocket_id
-   * @property {string} rocket_name
-   *
-   * @return {Promise<RocketBrief[]>}
-   * */
-  async getRockets() {
-    throw new Error("Not implemented");
-  }
+        return eventBriefs;
+    }
 
-  /**
-   * Должен возвращать информацию о запрошенной ракете
-   *
-   * @typedef {Object} RocketFull
-   * @property {number} rocket_id
-   * @property {string} rocket_name
-   * @property {string} first_flight
-   * @property {string} description
-   * @property {string} wikipedia
-   * @property {string[]} flickr_images
-   * Смотри источник данных:
-   * @property {Object} height
-   * @property {Object} diameter
-   * @property {Object} mass
-   * @property {Object} engines
-   * @property {Object} first_stage
-   * @property {Object} second_stage
-   *
-   * @param {string} id
-   * @return {Promise<RocketFull>}
-   * */
-  async getRocket(id) {
-    throw new Error("Not implemented");
-  }
+    /**
+     * Должен возвращать информацию о запрошенном событии
+     *
+     * @typedef {Object} EventFull
+     * @property {number} id
+     * @property {string} title
+     * @property {string} event_date_utc
+     * @property {string} details
+     * @property {Object.<string, ?string>} links
+     *
+     * @param {number} id
+     * @return {Promise<EventFull>}
+     * */
+    async getHistoryEvent(id) {
+        const response = await fetch(`https://api.spacexdata.com/v4/history/${id}`);
+        const eventData = await response.json();
 
-  /**
-   * Должен возвращать информацию о машине в космосе
-   *
-   * @typedef {Object} Roadster
-   * @property {string} name
-   * @property {string} launch_date_utc
-   * @property {string} details
-   * @property {number} earth_distance_km
-   * @property {number} mars_distance_km
-   * @property {string} wikipedia
-   *
-   * @return {Promise<Roadster>}
-   * */
-  async getRoadster() {
-    throw new Error("Not implemented");
-  }
+        return {
+            id: eventData.id,
+            title: eventData.title,
+            event_date_utc: eventData.event_date_utc,
+            details: eventData.details,
+            links: eventData.links
+        };
+    }
 
-  /**
-   * Должен возвращать информацию о всех посланных на Марс предметах
-   *
-   * @typedef {Object} Item
-   * @property {!string} id
-   * @property {!string} name
-   * @property {!string} phone
-   * @property {?number} weight
-   * @property {?string} color
-   * @property {?boolean} important
-   *
-   * @return {Promise<Item[]>}
-   * */
-  async getSentToMars() {
-    throw new Error("Not implemented");
-  }
 
-  /**
-   * Должен посылать на марс переданный предмет и
-   * возвращать информацию о всех посланных на Марс предметах
-   *
-   * @typedef {Object} ItemToSend
-   * @property {!string} name
-   * @property {!string} phone
-   * @property {?number} weight
-   * @property {?string} color
-   * @property {?boolean} important
-   *
-   * @param {ItemToSend} item
-   * @return {Promise<Item[]>}
-   * */
-  async sendToMars(item) {
-    throw new Error("Not implemented");
-  }
+    /**
+     * Должен возвращать информацию о всех ракетах
+     *
+     * @typedef {Object} RocketBrief
+     * @property {number} rocket_id
+     * @property {string} rocket_name
+     *
+     * @return {Promise<RocketBrief[]>}
+     * */
+    async getRockets() {
+        const response = await fetch('https://api.spacexdata.com/v4/rockets');
+        const data = await response.json();
 
-  /**
-   * Должен отменять отправку на марс переданного предмета и
-   * возвращать информацию о всех посланных на Марс предметах
-   *
-   * @param {Item} item
-   * @return {Promise<Item[]>}
-   * */
-  async cancelSendingToMars(item) {
-    throw new Error("Not implemented");
-  }
+        const rocketBriefs = data.map(rocket => ({
+            rocket_id: rocket.rocket_id,
+            rocket_name: rocket.rocket_name
+        }));
+
+        return rocketBriefs;
+    }
+
+    /**
+     * Должен возвращать информацию о запрошенной ракете
+     *
+     * @typedef {Object} RocketFull
+     * @property {number} rocket_id
+     * @property {string} rocket_name
+     * @property {string} first_flight
+     * @property {string} description
+     * @property {string} wikipedia
+     * @property {string[]} flickr_images
+     * Смотри источник данных:
+     * @property {Object} height
+     * @property {Object} diameter
+     * @property {Object} mass
+     * @property {Object} engines
+     * @property {Object} first_stage
+     * @property {Object} second_stage
+     *
+     * @param {string} id
+     * @return {Promise<RocketFull>}
+     * */
+    async getRocket(id) {
+        const response = await fetch(`https://api.spacexdata.com/v4/rockets/${id}`);
+        const rocketData = await response.json();
+
+        return {
+            rocket_id: rocketData.rocket_id,
+            rocket_name: rocketData.rocket_name,
+            first_flight: rocketData.first_flight,
+            description: rocketData.description,
+            wikipedia: rocketData.wikipedia,
+            flickr_images: rocketData.flickr_images,
+            height: rocketData.height,
+            diameter: rocketData.diameter,
+            mass: rocketData.mass,
+            engines: rocketData.engines,
+            first_stage: rocketData.first_stage,
+            second_stage: rocketData.second_stage
+        };
+    }
+
+    /**
+     * Должен возвращать информацию о машине в космосе
+     *
+     * @typedef {Object} Roadster
+     * @property {string} name
+     * @property {string} launch_date_utc
+     * @property {string} details
+     * @property {number} earth_distance_km
+     * @property {number} mars_distance_km
+     * @property {string} wikipedia
+     *
+     * @return {Promise<Roadster>}
+     * */
+    async getRoadster() {
+        const response = await fetch('https://api.spacexdata.com/v4/roadster');
+        const data = await response.json();
+
+        return {
+            name: data.name,
+            launch_date_utc: data.launch_date_utc,
+            details: data.details,
+            earth_distance_km: data.earth_distance_km,
+            mars_distance_km: data.mars_distance_km,
+            wikipedia: data.wikipedia
+        };
+    }
+
+    /**
+     * Должен возвращать информацию о всех посланных на Марс предметах
+     *
+     * @typedef {Object} Item
+     * @property {!string} id
+     * @property {!string} name
+     * @property {!string} phone
+     * @property {?number} weight
+     * @property {?string} color
+     * @property {?boolean} important
+     *
+     * @return {Promise<Item[]>}
+     * */
+    async getSentToMars() {
+        const response = await fetch('https://api.spacexdata.com/v4/sent_to_mars');
+        const data = await response.json();
+
+        return data;
+    }
+
+    /**
+     * Должен посылать на марс переданный предмет и
+     * возвращать информацию о всех посланных на Марс предметах
+     *
+     * @typedef {Object} ItemToSend
+     * @property {!string} name
+     * @property {!string} phone
+     * @property {?number} weight
+     * @property {?string} color
+     * @property {?boolean} important
+     *
+     * @param {ItemToSend} item
+     * @return {Promise<Item[]>}
+     * */
+    async sendToMars(item) {
+        throw new Error("Not implemented");
+    }
+
+    /**
+     * Должен отменять отправку на марс переданного предмета и
+     * возвращать информацию о всех посланных на Марс предметах
+     *
+     * @param {Item} item
+     * @return {Promise<Item[]>}
+     * */
+    async cancelSendingToMars(item) {
+        throw new Error("Not implemented");
+    }
 }
--- server.mjs
+++ server.mjs
@@ -7,21 +7,87 @@ import bodyParser from "body-parser";
 import fetch from "node-fetch";
 
 const rootDir = process.cwd();
-const port = 3000;
+const port = 3000;//f
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.static('spa/build'));
 
-app.get("/client.mjs", (_, res) => {
-  res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
-  res.sendFile(path.join(rootDir, "client.mjs"), {
-    maxAge: -1,
-    cacheControl: false,
-  });
+app.use((req, res, next) => {
+    const excludePaths = ['/api', '/static', '/login'];
+    const isIndexRoute = req.path.includes('index.html');
+
+    if (!excludePaths.some(path => req.path.includes(path)) && !req.cookies.username && isIndexRoute) {
+        return res.redirect('/login');
+    }
+    next();
 });
 
 app.get("/", (_, res) => {
-  res.send(":)");
+    res.send(":)");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+app.get("/client.mjs", (_, res) => {
+    res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+    res.sendFile(path.join(rootDir, "client.mjs"), {
+        maxAge: -1,
+        cacheControl: false,
+    });
 });
+
+app.use(express.static('spa/build'));
+
+app.get('/api/rockets', async (req, res) => {
+    try {
+        const response = await fetch('https://api.spacexdata.com/v4/rockets');
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({message: 'Internal Server Error'});
+    }
+});
+
+app.post('/api/login', (req, res) => {
+    const user = req.body.username;
+    res.cookie("username", user, {httpOnly: true, secure: true, sameSite: "strict"});
+    res.json({username: user || null});
+});
+
+app.post('/api/logout', (req, res) => {
+    res.clearCookie('username');
+    res.sendStatus(200);
+});
+
+app.post('/api/sendToMars', (req, res) => {
+    res.sendStatus(123);
+});
+
+app.get('/api/user', (req, res) => {
+    const user = req.cookies["username"];
+    res.json({username: user || null});
+});
+
+app.get("/", (_, res) => {
+    res.send(":)");
+});
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve("spa/build/index.html"));
+});
+
+https
+    .createServer(
+        {
+            key: fs.readFileSync("certs/server.key"),
+            cert: fs.readFileSync("certs/server.cert"),
+        },
+        app
+    )
+    .listen(port, function () {
+        console.log(
+            `App listening on port ${port}`
+        );
+    });
+
